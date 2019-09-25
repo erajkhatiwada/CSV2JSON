c6e101afad9f3dfa5d0e8959256708333d1d9a39
erajkhatiwada <32199662+erajkhatiwada@users.noreply.github.com>
2019-09-24 22:38:41 -0500

    csv to json and download done. need to work on layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,7 @@ export class HomeComponent implements OnInit {
   convertFile(csv: any) {
 
     let reader: FileReader = new FileReader();
+
     this.fileReaded = csv.target.files[0];
     console.log(this.fileReaded);
 
@@ -24,9 +25,41 @@ export class HomeComponent implements OnInit {
 
     reader.onload = (e) => {
       let csv = reader.result;
-      let arr = csv.toString().split(/\r|\n|\r/);
-      console.log(arr);
+      let arr  = csv.toString().split(/\r|\n|\r/);
+
+      let property = arr[0].split(',');
+      console.log(arr,"Main array");
+      console.log(property,"Properties");
+
+      let jsonArray = [];
+
+      for(let index in arr){
+        if(parseInt(index) != 0){
+          if(arr[index] !== ""){
+            let obj = arr[index].split(',');
+            let jsonObj = {};
+            for(let propIndex in property){
+              jsonObj[property[propIndex]] = obj[propIndex];
+            }
+            jsonArray.push(jsonObj);
+          }
+        }
+      }
+
+      console.log(jsonArray, "JSON array");
+       
+      const a = document.createElement("a");
+      a.style.display = "none";
+      document.body.appendChild(a);
+
+      let x = new Blob([JSON.stringify(jsonArray)],{type: 'application/json'});
+      a.href = URL.createObjectURL(x);
+
+      a.download = 'download.json';
+      a.click();
+   
+
     } 
-    
+
   }
 }
